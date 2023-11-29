b7af809ed9697a49c545801347182a50463ddb7e
Catalina <yangluojiashan678@163.com>
2023-11-29 11:04:12 +0800

    sort--version2

--- src/main/java/com/catalina/git/GitTest.java
+++ src/main/java/com/catalina/git/GitTest.java
@@ -14,7 +14,7 @@ public class GitTest {
         boolean flag = false;
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j+1]){
+                if (arr[j] < arr[j+1]){
                     flag = true;
                     int temp = arr[j];
                     arr[j] = arr[j+1];
@@ -27,6 +27,24 @@ public class GitTest {
                 flag = false;
             }
         }
-        System.out.println("排序后的数组为："+Arrays.toString(arr));
+        System.out.println("升序排序后的数组为："+Arrays.toString(arr));
+        insertSort(arr);
+        System.out.println("降序排序后的数组为："+Arrays.toString(arr));
+    }
+
+    /**
+     * 插入排序
+     * @param arr：要排序的数组
+     */
+    public static void insertSort(int[] arr){
+        for (int i = 1; i < arr.length; i++){
+            int insertValue = arr[i];
+            int insertIndex = i  - 1;
+            while (insertIndex >= 0 && insertValue < arr[insertIndex]){
+                arr[insertIndex+1] = arr[insertIndex];
+                insertIndex --;
+            }
+            arr[insertIndex+1] = insertValue;
+        }
     }
 }
